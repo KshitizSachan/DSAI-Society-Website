ba211233fd7187702450fd9421da272b67f4c76d
Chirag Mittal <chirag.workid@gmail.com>
2021-12-30 00:11:32 +0530

    animation not starting bug solved

--- src/MainHome.jsx
+++ src/MainHome.jsx
@@ -15,7 +15,7 @@ function Home(){
                     <button type="button" className="btn btn-lg rounded-pill btn-default" style={{paddingLeft:40,paddingRight:40,color: "white",borderColor:"#5E5D8C"}}>Know More</button>
                 </div>
                 <div className="col-lg-6">
-                    <video src="../Videos/animation.mp4" autoPlay={true} loop={true} width={450} className="homeAnimation"/>
+                    <video src="../Videos/animation.mp4" muted autoPlay={true} loop={true} width={450} className="homeAnimation"/>
                 </div> 
             </div>
             <FontAwesomeIcon className="arrowIcon" size="4x" icon={faAngleDoubleDown}/>
