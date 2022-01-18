0060ae54f8da24667ff0789962ca68d0ebd97667
Ayush <ani9431619703@gmail.com>
2022-01-19 02:12:34 +0530

    Adding the carousel route

--- src/Components/Blog.jsx
+++ src/Components/Blog.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
-import Wowdata from "../RecieveData/data";
 
 function Blog(){
     return (
@@ -9,7 +8,6 @@ function Blog(){
             <Navbar />
             <hr className="rule-styling"></hr> 
             <h1>Blog</h1>
-            <Wowdata></Wowdata>
             <Footer />
         </div>
     );
--- src/Components/newsAndEvents.jsx
+++ src/Components/newsAndEvents.jsx
@@ -1,69 +1,90 @@
 import React from "react";
 import CarasolItem from "./carasoulItem.jsx";
 import { useEffect, useState } from "react";
-import data from '../RecieveData/data'
 import axios from "axios";
 
-function NewsAnnouncements(){
+function NewsAnnouncements() {
 
     const [news, changenewsdata] = useState([])
+    const [announcements, changeannoucedata] = useState([])
 
     useEffect(async () => {
         try {
             await axios({
-              method: "get",
-              url: "https://dsai-serverside-code.herokuapp.com/news",
-              headers: {},
+                method: "get",
+                url: "https://dsai-serverside-code.herokuapp.com/news",
+                headers: {},
             }).then(function (response) {
-              changenewsdata(response.data)
+                changenewsdata(response.data)
             });
-          } catch (error) {
+        } catch (error) {
             console.error(error);
-          }
-      }, []);
+        }
+    }, []);
 
-    
+    useEffect(async function getUser() {
+        try {
+            await axios({
+                method: "get",
+                url: "https://dsai-serverside-code.herokuapp.com/home/announcements",
+                headers: {},
+            }).then(function (response) {
+                changeannoucedata(response.data);
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }, [])
+
+    useEffect(() => { })
+
     return (
         <div className="newsAndAnnouncements">
             <h1 className="newsEventsH1">News And Events</h1>
             <div className="row">
-                    <div className="col-lg-6">
-                        <h1 style={{textAlign:"center",marginTop:"5%",color:"#D17CFF"}}>Latest News</h1>
-                            <div className="divNewsEvents">
-                                <ul>
-                                    {news.map((newsitem) => <li>{newsitem.title}</li>)}
-                                </ul>
+                <div className="col-lg-6">
+                    <h1 style={{ textAlign: "center", marginTop: "5%", color: "#D17CFF" }}>Latest News</h1>
+                    <div className="divNewsEvents">
+                        <ul>
+                            {news.map((newsitem) => <li>{newsitem.title}</li>)}
+                        </ul>
+                    </div>
+                </div>
+                <div className="col-lg-6">
+                    <h1 style={{ textAlign: "center", marginTop: "5%", color: "#D17CFF" }}>Upcoming Events</h1>
+                    <div className="divNewsEvents">
+                        <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+                            <ol class="carousel-indicators">
+                                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                            </ol>
+                            <div className="carousel-inner">
+                                {announcements.map((announcement) => {
+                                    if (announcements[0] === announcement) {
+                                        return <div className="carousel-item active">
+                                            <CarasolItem heading={announcement.title} description={announcement.date} link={announcement.link} />
+                                        </div>
+                                    }
+                                    return <div className="carousel-item">
+                                        <CarasolItem heading={announcement.title} description={announcement.date} link={announcement.link} />
+                                    </div>
+                                })}
                             </div>
+                            <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Previous</span>
+                            </a>
+                            <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                                <span className="sr-only">Next</span>
+                            </a>
+                        </div>
                     </div>
-                    <div className="col-lg-6">
-                    <h1 style={{textAlign:"center",marginTop:"5%",color:"#D17CFF"}}>Upcoming Events</h1>
-                            <div className="divNewsEvents">
-                                <div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-                                    <div className="carousel-inner">
-                                        <div className="carousel-item active first-item">
-                                            <CarasolItem heading="#66 Days Of Code" description="Starting at some date"/>
-                                        </div>
-                                        <div className="carousel-item second-item">
-                                            <CarasolItem heading="#66 Days Of Code" description="Starting at some date"/>
-                                        </div>
-                                        <div className="carousel-item third-item">
-                                            <CarasolItem heading="#66 Days Of Code" description="Starting at some date"/>
-                                        </div>
-                                    </div>
-                                    <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                                        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                                        <span className="sr-only">Previous</span>
-                                    </a>
-                                    <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                                        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                                        <span className="sr-only">Next</span>
-                                    </a>
-                                    </div>
-                                </div>
-                    </div>
+                </div>
             </div>
         </div>
     );
 }
- 
+
 export default NewsAnnouncements;
